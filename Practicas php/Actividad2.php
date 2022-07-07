dbc7eb4f7f595e80fd9ee3bef73be675e667b203
Serbin <105952577+SerbinLD@users.noreply.github.com>
2022-07-07 17:44:10 -0500

    Unidad III. Tarea 2

---
+++ Practicas php/Actividad2.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+    <?php
+    $x = 20;
+    $y = 10;
+
+    echo $x + $y;
+
+    echo "<br>"
+      
+    ?>
+    <?php 
+
+    $x = 2.8;
+    $y = 1.7;
+
+    echo $x + $y; 
+
+    echo "<br>"
+    ?>
+
+<?php 
+
+$x = 2;
+$y = 8;
+
+echo $x * $y; 
+
+echo "<br>"
+
+?>
+
+<?php
+$var1='Serbin';
+$var2=' 11';
+$texto_completo = $var1 . $var2;
+echo $texto_completo;
+
+?>
+
+ </body>
+</html> 
